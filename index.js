44eeffdf951802cae5d2b4fbabea191d53b82567
0MollyMan0 <fougereantoine0@gmail.com>
2025-05-26 09:33:22 +0200

    project v1

--- index.js
+++ index.js
@@ -24,6 +24,29 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// Without params
+app.get("/api", (req, res) => {
+  const now = new Date();
+  res.json({ unix: now.getTime(), utc: now.toUTCString() });
+});
+
+app.get("/api/:date?", function(req, res) {
+  const date1 = new Date(req.params.date); // Prepare the const for a UTC req
+  const date2 = new Date(Number(req.params.date)); // Prepare the const for a unix req
+  // Test if it is a UTC req
+  if (isNaN(date2.getTime())) {
+    res.json({ unix: date1.getTime(), utc: date1.toUTCString() });
+  }
+  // Test if it is unix req
+  else if (isNaN(date1.getTime())) {
+    res.json({ unix: date2.getTime(), utc: date2.toUTCString() });
+  }
+  // A real invalid date
+  else {
+    res.json({ error: "Invalid Date" });
+  }
+});
+
 
 
 // Listen on port set in environment variable or default to 3000
